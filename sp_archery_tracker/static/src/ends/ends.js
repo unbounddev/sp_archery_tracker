efb341dc13e8b630cd0467f0edada31bc7e212bf
unbounddev <unbounddev@git>
2025-11-05 11:49:32 -0800

    Add functionality to display and add ends

--- sp_archery_tracker/static/src/ends/ends.js
+++ sp_archery_tracker/static/src/ends/ends.js
@@ -1,14 +1,41 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { standardFieldProps } from "@web/views/fields/standard_field_props";
+
+const MEASUREMENT_UNITS = {
+  METER: "m",
+  FEET: "ft",
+  YARDS: "yd"
+}
+
 export class Ends extends Component {
   static template = "sp_archery_tracker.Ends";
   static props = {...standardFieldProps};
   setup() {
-    console.log(this.props.record.data.ends.ends)
-    console.log(this.props)
+    // console.log(this.props.record.data.ends.ends)
+    if (!this.value || !this.value.ends){
+      this.setDefault();
+    }
 
   }
+  get value(){
+    return this.props.record.data.ends;
+  }
+  async setDefault() {
+      try {
+        await this.props.record.update({ ends: { ends: [] } }, { save: true });
+      }
+      catch(e) {
+        console.log("ERROR: Couldn't set default value");
+      }
+  }
+  async addEnd() {
+    try {
+      await this.props.record.update({ ends: { ends: [...this.value.ends, { distance: 20, unit: MEASUREMENT_UNITS.YARDS, arrows: [] }] } }, { save: true });
+    } catch (e) {
+      console.log("ERROR: Could not add end")
+    }
+  } 
 }
 
 export const endsField = {
