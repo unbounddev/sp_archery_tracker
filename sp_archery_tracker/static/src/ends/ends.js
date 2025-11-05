4d1ed5f5af8801338878a78d550484bca6b1c58a
unbounddev <unbounddev@git>
2025-11-05 15:45:26 -0800

    Add functionality to add and display arrows

--- sp_archery_tracker/static/src/ends/ends.js
+++ sp_archery_tracker/static/src/ends/ends.js
@@ -1,4 +1,4 @@
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { standardFieldProps } from "@web/views/fields/standard_field_props";
 
@@ -13,10 +13,14 @@ export class Ends extends Component {
   static props = {...standardFieldProps};
   setup() {
     // console.log(this.props.record.data.ends.ends)
+    this.state = useState({
+      selectedArrow: null,
+      showEditArrowDialog: true
+    })
     if (!this.value || !this.value.ends){
       this.setDefault();
     }
-
+    this.addArrow = this.addArrow.bind(this);
   }
   get value(){
     return this.props.record.data.ends;
@@ -36,6 +40,16 @@ export class Ends extends Component {
       console.log("ERROR: Could not add end")
     }
   } 
+  async addArrow(end) {
+    console.log(this)
+    try {
+      const ends = JSON.parse(JSON.stringify(this.value.ends));
+      ends[end].arrows.push(0);
+      await this.props.record.update({ ends: { ends }}, { save: true });
+    } catch (e) {
+      console.log("ERROR: Could not add arrow", e)
+    }
+  }
 }
 
 export const endsField = {
