8d5dbe1ad2726ee648824dced001122537ceeb35
unbounddev <unbounddev@git>
2025-11-06 17:13:13 -0800

    Add X's and 10's field to log

--- sp_archery_tracker/models/sp_archery_tracker_log.py
+++ sp_archery_tracker/models/sp_archery_tracker_log.py
@@ -7,12 +7,16 @@ class SPArcheryTrackerLog(models.Model):
     date = fields.Datetime(default=fields.Datetime.now())
     user_id = fields.Many2one("res.users", string="User")
     ends = fields.Json(default={"ends": []})
-    score = fields.Integer(compute="_compute_score", store=True)
+    score = fields.Integer(compute="_compute_stats", store=True)
+    num_of_tens = fields.Integer(compute="_compute_stats", store=True, string="10's")
+    num_of_x = fields.Integer(compute="_compute_stats", store=True, string="X's")
 
     @api.depends("ends")
-    def _compute_score(self):
+    def _compute_stats(self):
         for record in self:
             new_score = 0
+            num_of_x = 0
+            num_of_tens = 0
             for end in record.ends['ends']:
                 for arrow in end['arrows']:
                     arrow_value = 0
@@ -21,8 +25,13 @@ class SPArcheryTrackerLog(models.Model):
                     except:
                         if arrow == "X":
                             arrow_value = 10
+                            num_of_x += 1
                         else:
                             arrow_value = 0
-                    new_score = new_score + arrow_value
+                    if arrow_value == 10:
+                        num_of_tens += 1
+                    new_score += arrow_value
 
             record.score = new_score
+            record.num_of_x = num_of_x
+            record.num_of_tens = num_of_tens
